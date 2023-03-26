864c002f56522d21639f0f37914e2dd301e7a9f3
cynthia <cynthiakagz@gmail.com>
2023-03-26 21:46:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+    const cats = [
+        "Milo",
+        "Otis",
+        "Garfield",
+    ];
+    function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph);
+    }
+    function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob);
+}
+    function destructivelyRemoveLastCat(Garfield) {
+    cats.pop(Garfield);
+}
+    function destructivelyRemoveFirstCat(Milo) {
+    cats.shift(Milo);
+}
+    function nondestructivelyAppendCat(Broom) {
+       cats.push(Broom);
+}
+    function nondestructivelyPrependCat(Arnold) {
+cats.unshift(Arnold);
+}
+    function nondestructivelyRemoveLastCat(Garfield) {
+cats.pop(Garfield);
+}
+    function nondestructivelyRemoveFirstCat(Milo) {
+    cats.shift(Milo);
+}
+
+   
+
