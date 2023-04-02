5956c9522421e6a05855c87270615765dfd59b73
cynthia <cynthiakagz@gmail.com>
2023-04-02 23:09:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -25,9 +25,33 @@ cats.unshift(Arnold);
     function nondestructivelyRemoveLastCat(Garfield) {
 cats.pop(Garfield);
 }
-    function nondestructivelyRemoveFirstCat(Milo) {
+function nondestructivelyRemoveFirstCat(Milo) {
     cats.shift(Milo);
 }
+function appendCat(Broom) {
+    return[...cats,Broom];
+}
+function prependCat(Arnold) {
+    
+    return[Arnold,...cats];
+}
+function removeLastCat() {
+    
+        const remove = cats.slice(0,2);
+        return remove;
+    }
+    
+        //const remove = cats.slice(Garfield);
+        //return remove;
+    
+    //return [Milo,Otis,Garfield]
+    //cats.slice(Garfield);
 
+function removeFirstCat() {
+    
+        const first = cats.slice(1, 3);
+        return first;
+    }
+    //return [Milo,Otis,Garfield];
+   //cats.slice(Milo);
    
-
